83039af89c0a9ab854ff45a51bd8dfd59b5a23eb
Khushal Rathi <khushalrathi200416@gmail.com>
2025-05-11 02:57:21 +0530

    postgres SQL

--- w_17/src/conectdb.ts
+++ w_17/src/conectdb.ts
@@ -1,5 +1,5 @@
 import { Client } from "pg"
-const client = new Client("postgresql://neondb_owner:npg_eTYdHhyNu7t3@ep-curly-rain-a4vdd0sn-pooler.us-east-1.aws.neon.tech/neondb?sslmode=require");
+const client = new Client("postgresql://*********************@ep-curly-rain-a4vdd0sn-pooler.us-east-1.aws.neon.tech/neondb?sslmode=require");
 
 
 export const connectDb = async() => {
