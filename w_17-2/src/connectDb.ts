a4c8a7808914812a4d872dcfd96d76f8e195f08d
Khushal Rathi <khushalrathi200416@gmail.com>
2025-05-12 04:09:28 +0530

    Relations,Join,Transactions in postgres SQL

--- w_17-2/src/connectDb.ts
+++ w_17-2/src/connectDb.ts
@@ -1,5 +1,5 @@
 import { Client } from "pg"
-const pgclient = new Client("postgresql://neondb_owner:npg_eTYdHhyNu7t3@ep-curly-rain-a4vdd0sn-pooler.us-east-1.aws.neon.tech/neondb?sslmode=require");
+const pgclient = new Client("postgresql://neondb_owner:**********@ep-curly-rain-a4vdd0sn-pooler.us-east-1.aws.neon.tech/neondb?sslmode=require");
 
 
 export const connectDb = async() => {
